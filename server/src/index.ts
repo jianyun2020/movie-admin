490dc3b10a3269c03ba7ecbfbc33e6598f1c1451
Jianyun <sujianyun2020@163.com>
2024-07-04 06:17:46 +0800

    处理plainobject的转换

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,27 @@
 import { validate } from "class-validator";
 
 import { Movie } from "./entities/Movie";
+import { plainToClass } from "class-transformer";
 
 
-const mv = new Movie();
+const mv: any = {}; // plain Object
 
-mv.name = '金刚';
-mv.types = ['喜剧'];
-mv.areas = ['中国大陆'];
+mv.name = 2222;
+mv.types = ["22"];
+mv.areas = [1];
 mv.timeLong = 120;
 mv.isHot = true;
 mv.isClassic = true;
 mv.isComing = true; 
+mv.test = 1;
 
-validate(mv).then(errors => {
+// 将plain Object 转换成 Movie 的对象
+
+const movie = plainToClass(Movie, mv as object);
+
+console.log(movie);
+
+validate(movie).then(errors => {
   if (errors.length > 0) {
     console.log("validation failed. errors: ", errors);
   } else {
